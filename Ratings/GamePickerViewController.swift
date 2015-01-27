3b2daac9016b4d57dcf3ca1a2c44e013a96995d0
“KEEVIN <keevin.mitchell@gmail.com>
2015-01-27 16:41:05 -0500

    The Game Picker Screen with working function

--- Ratings/GamePickerViewController.swift
+++ Ratings/GamePickerViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class GamePickerViewController: UITableViewController {
-    
+    //properties to hold the name and the index of the currently selected game
+    var selectedGame:String? = nil
+    var selectedGameIndex:Int? = nil
+    //
     var games:[String]!
     
     override func viewDidLoad() {
@@ -41,9 +44,42 @@ class GamePickerViewController: UITableViewController {
         return games.count
     }
     
+    
+    /*
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("GameCell", forIndexPath: indexPath) as UITableViewCell
         cell.textLabel.text = games[indexPath.row]
         return cell
     }
+*/
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        //This sets a checkmark on the cell that contains the name of the currently selected game. Small gestures such as these will be appreciated by the users of the app.
+        let cell = tableView.dequeueReusableCellWithIdentifier("GameCell", forIndexPath: indexPath) as UITableViewCell
+        cell.textLabel.text = games[indexPath.row]
+        
+        if indexPath.row == selectedGameIndex {
+            cell.accessoryType = .Checkmark
+        } else {
+            cell.accessoryType = .None
+        }
+        return cell
+    }
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+        
+        //Other row is selected - need to deselect it
+        if let index = selectedGameIndex {
+            let cell = tableView.cellForRowAtIndexPath(NSIndexPath(forRow: index, inSection: 0))
+            cell?.accessoryType = .None
+        }
+        
+        selectedGameIndex = indexPath.row
+        selectedGame = games[indexPath.row]
+        
+        //update the checkmark for the current row
+        let cell = tableView.cellForRowAtIndexPath(indexPath)
+        cell?.accessoryType = .Checkmark
+    }
+    
+    
 }
