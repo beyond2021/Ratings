647993347fc2f3f2a1e1f18c793919881fe4a612
“KEEVIN <keevin.mitchell@gmail.com>
2015-01-27 13:17:24 -0500

    Clean up app structure and add iAd integration.

---
+++ Ratings/PlayerCell.swift
@@ -0,0 +1,24 @@
+//
+//  PlayerCell.swift
+//  Ratings
+//
+//  Created by KEEVIN MITCHELL on 1/27/15.
+//  Copyright (c) 2015 Beyond 2021. All rights reserved.
+//
+
+import UIKit
+
+class PlayerCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Ratings/PlayersViewController.swift
+++ Ratings/PlayersViewController.swift
@@ -7,12 +7,18 @@
 //
 
 import UIKit
+import iAd
 
 class PlayersViewController: UITableViewController {
     
     var players: [Player] = playersData //Here we add a player array property
     /*
     We could simply have set up the sample data in PlayersViewController when defining the players variable above. But because this data might later be provided from a plist or an SQL file, it’s wise to handle loading the data outside of the view controller.*/
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.canDisplayBannerAds = true
+    }
     
     // MARK: - TableView DataCource Methods
     
